0a93eb9b2c87cdc7356b51370829ebd2e2391ec3
Jhonathan Pablo <jhonathan.pablo.ss321@gmail.com>
2025-08-12 10:18:13 -0300

    Update project.py

--- project.py
+++ project.py
@@ -1,16 +1,19 @@
-nota = input('Digite sua nota: ')
+while True:
+    print('Bem-vindo a lista de TAREFAS!')
+    
+    if nota >= 90:
+        print('Parabens')
 
-if nota >= 90:
-    print('Parabens')
+    elif nota >= 80:
+        print('Muito bem')
 
-elif nota >= 80:
-    print('Muito bem')
+    elif nota >= 70:
+        print('Bom, bom')
+
+    elif nota >= 55:
+        print('Foi por pouco')
+
+    elif nota <= 54:
+        print('Precisa melhorar')
+
     
-elif nota >= 70:
-    print('Bom, bom')
-    
-elif nota >= 55:
-    print('Foi por pouco')
-    
-elif nota <= 54:
-    print('Precisa melhorar')
