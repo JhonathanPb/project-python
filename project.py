22f3ca36e10a7ca469405d9f87c90864f5f4a60e
Jhonathan Pablo <jhonathan.pablo.ss321@gmail.com>
2025-08-12 11:03:20 -0300

    CHANGES

--- project.py
+++ project.py
@@ -1,19 +1,54 @@
+tarefa = []
+
 while True:
-    print('Bem-vindo a lista de TAREFAS!')
+    print('Bem-vindo a Gerenciador de TAREFAS!')
+    print('Escolha uma opção:')
+    print('1. Adicionar Tarefa')
+    print('2. Ver tarefas')
+    print('3. Remover uma Tarefa')
+    print('4. Sair')
+    resp = int(input())
     
-    if nota >= 90:
-        print('Parabens')
+    # Addicionar Tarefas
+    if resp == 1:
+        
+        print('Digite a nova Tarefa: ')
+        tarefa.append(input())
+        
+    # Ver Tarefas
+    elif resp == 2:
+        if not tarefa:
+            print('Não há tarefas adicionadas!')
+            add_tarefa = input('\nDeseja adicionar uma tarefa? s/n')
+            if add_tarefa.lower() == 's':
+                print('Digite a nova Tarefa: ')
+                tarefa.append(input())
+        for tarefa in tarefa:
+            print(tarefa)
+            
+    # Remover Tarefas
+    elif resp == 3:
+        if not tarefa:
+            print('Não há tarefas adicionadas!')
+            add_tarefa = input('\nDeseja adicionar uma tarefa? s/n')
+            if add_tarefa.lower() == 's':
+                print('Digite a nova Tarefa: ')
+                tarefa.append(input())
+            
+            
+        else:
+            print(tarefa)
+            print('Digite o item que deseja remover?')
+            tarefa.discard(input())
+            print(tarefa)
 
-    elif nota >= 80:
-        print('Muito bem')
-
-    elif nota >= 70:
-        print('Bom, bom')
-
-    elif nota >= 55:
-        print('Foi por pouco')
-
-    elif nota <= 54:
-        print('Precisa melhorar')
-
+    # Sair
+    elif resp == 4:
+        sair = input('Deseja realmente sair? s/n')
+        if sair.lower() == 'n':
+            print('Obrigado por usar o Gerenciador de Tarefas!')
+            break
+    
+    else:
+        print('Opção Inválida!')
     
