2f8e8ea113b026f23639741a7a507284b90ebd0c
Jhonathan Pablo <jhonathan.pablo.ss321@gmail.com>
2025-08-12 10:03:11 -0300

    Create project.py

---
+++ project.py
@@ -0,0 +1,16 @@
+nota = input('Digite sua nota: ')
+
+if nota >= 90:
+    print('Parabens')
+
+elif nota >= 80:
+    print('Muito bem')
+    
+elif nota >= 70:
+    print('Bom, bom')
+    
+elif nota >= 55:
+    print('Foi por pouco')
+    
+elif nota <= 54:
+    print('Precisa melhorar')
