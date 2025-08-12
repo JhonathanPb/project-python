d235dbb9e740267799f7187c3accfe76280d5cf7
Jhonathan Pablo <jhonathan.pablo.ss321@gmail.com>
2025-08-12 11:51:45 -0300

    Update project.py

--- project.py
+++ project.py
@@ -1,54 +1,65 @@
 tarefa = []
 
+def add_tarefas():
+    print('Digite a nova Tarefa: ')
+    tarefa.append(input())
+    
+def lista_vazia():
+    if not tarefa:
+            print('Não há tarefas adicionadas!')
+            simounao = input('\nDeseja adicionar uma tarefa? s/n')
+            if simounao.lower() == 's':
+                add_tarefas()
+    else:
+        print(tarefa)
+    
+
 while True:
     print('Bem-vindo a Gerenciador de TAREFAS!')
-    print('Escolha uma opção:')
+    print('Opções 1 a 4:')
     print('1. Adicionar Tarefa')
     print('2. Ver tarefas')
     print('3. Remover uma Tarefa')
     print('4. Sair')
-    resp = int(input())
     
-    # Addicionar Tarefas
-    if resp == 1:
+    
+    try:
+        resp = int(input('Escolha uma opção: '))
         
-        print('Digite a nova Tarefa: ')
-        tarefa.append(input())
+        # Addicionar Tarefas
+        if resp == 1:
+            add_tarefas()
+            
+        # Ver Tarefas
+        elif resp == 2:
+            lista_vazia()
+                
+        # Remover Tarefas
+        elif resp == 3:
+            while True:
+                lista_vazia()       
+                print('Digite o item que deseja remover?')
+                item_rem = input()
+                if item_rem in tarefa:
+                    tarefa.remove(item_rem)
+                else:
+                    print('Item não encontrado!')
+                    simounao = input('Deseja digitar novamente? s/n')
+                    if simounao.lower() == 's':
+                        continue
+                    else:
+                        break
+        # Sair
+        elif resp == 4:
+            sair = input('Deseja realmente sair? s/n')
+            if sair.lower() == 'n':
+                print('Obrigado por usar o Gerenciador de Tarefas!')
+                break
         
-    # Ver Tarefas
-    elif resp == 2:
-        if not tarefa:
-            print('Não há tarefas adicionadas!')
-            add_tarefa = input('\nDeseja adicionar uma tarefa? s/n')
-            if add_tarefa.lower() == 's':
-                print('Digite a nova Tarefa: ')
-                tarefa.append(input())
-        for tarefa in tarefa:
-            print(tarefa)
-            
-    # Remover Tarefas
-    elif resp == 3:
-        if not tarefa:
-            print('Não há tarefas adicionadas!')
-            add_tarefa = input('\nDeseja adicionar uma tarefa? s/n')
-            if add_tarefa.lower() == 's':
-                print('Digite a nova Tarefa: ')
-                tarefa.append(input())
-            
-            
         else:
-            print(tarefa)
-            print('Digite o item que deseja remover?')
-            tarefa.discard(input())
-            print(tarefa)
-
-    # Sair
-    elif resp == 4:
-        sair = input('Deseja realmente sair? s/n')
-        if sair.lower() == 'n':
-            print('Obrigado por usar o Gerenciador de Tarefas!')
-            break
-    
-    else:
-        print('Opção Inválida!')
-    
+            print('Opção Inválida!')
+        
+        
+    except ValueError:
+     print('Isso não é um opção válida. Por favor, digite uma das opções.')
+        
